fc457b1b64cf53573b006e116d8925288295a6f2
alvarocopa <alvaro-copa2011@hotmail.com>
2023-08-04 20:06:59 -0300

    cambios en las vista home  para que sea responsive

--- app.js
+++ app.js
@@ -9,5 +9,10 @@ app.get('/', function (req, res) {
 app.get('/detalleProd', function (req, res) {
    res.sendFile(path.join(__dirname,'./views/detalleProd.html'));
 })
-
+app.get('/login', function (req, res) {
+   res.sendFile(path.join(__dirname,'./views/login.html'));
+})
+app.get('/registro', function (req, res) {
+   res.sendFile(path.join(__dirname,'./views/registro.html'));
+})
 app.listen(3001, ()=> console.log('servidor corriendo'))
