0e42f6a9e02e30e89e6dbb456de63c15b860d778
alvarocopa <alvaro-copa2011@hotmail.com>
2023-08-23 18:15:23 -0300

    agregando la vista agregarprod y css

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,7 +5,10 @@ const path = require('path');
 const indexControllers = (req,res) =>{
     res.render('index');
 }
-
+const agregarProdControllers = (req,res) =>{
+    res.render('agregar-product');
+}
 module.exports = {
     indexControllers,
+    agregarProdControllers,
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { indexControllers, loginControllers, registerControllers } = require('../controllers');
+const { indexControllers, loginControllers, registerControllers, agregarProdControllers } = require('../controllers');
 
 const router = express.Router();
 
 router.get('/',indexControllers)
-
+router.get('/agregar-producto',agregarProdControllers)
 module.exports = router;
