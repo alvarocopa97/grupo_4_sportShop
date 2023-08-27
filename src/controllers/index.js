33ab47c9455762414b564999aa843856212c9f08
alvarocopa <alvaro-copa2011@hotmail.com>
2023-08-27 18:55:39 -0300

    uniendo los ejs y modificando algunos estilos para una mejor interfaz

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -8,7 +8,23 @@ const indexControllers = (req,res) =>{
 const agregarProdControllers = (req,res) =>{
     res.render('agregar-product');
 }
+const loginControllers = (req,res) =>{
+    res.render('login');
+}
+const registerControllers = (req,res) =>{
+    res.render('registro');
+}
+const detalleProdControllers = (req,res) =>{
+    res.render('detalleProd');
+}
+const productCartControllers = (req,res) =>{
+    res.render('productCart');
+}
 module.exports = {
     indexControllers,
     agregarProdControllers,
+    loginControllers,
+    registerControllers,
+    detalleProdControllers,
+    productCartControllers,
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { indexControllers, loginControllers, registerControllers, agregarProdControllers } = require('../controllers');
+const { indexControllers, loginControllers, registerControllers, agregarProdControllers, detalleProdControllers,productCartControllers} = require('../controllers');
 
 const router = express.Router();
 
 router.get('/',indexControllers)
 router.get('/agregar-producto',agregarProdControllers)
+router.get('/login',loginControllers)
+router.get('/registro',registerControllers)
+router.get('/detalleProd',detalleProdControllers)
+router.get('/productCart',productCartControllers)
+
 module.exports = router;
