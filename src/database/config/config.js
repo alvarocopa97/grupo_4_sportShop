248a9c8dfa43119b4a0a732a43e4f057b41e5a4f
Maria-Vedia <vediamarii@gmail.com>
2023-12-12 10:58:18 -0300

    modificaciones en la lista de productos

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports={
   "development": {
     "username": "root",
-    "password": "root",
+    "password": "",
     "database": "shop_db",
     "host": "localhost",
     "dialect": "mysql"
